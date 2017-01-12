1a212760a851a24a3d6b39fc6e1f4ea4c64bbc99
Michael T. Gillespie <mtgillespie@facettech.com>
2017-01-12 11:18:58 -0600

    Fixed scan example, added new examples.

--- examples/file.php
+++ examples/file.php
@@ -1,8 +1,16 @@
 <?php
 require_once '../vendors/autoload.php';
-$apiKey = 'secret';
-$file = new Malwr\File($apiKey);
-$resourceObj = $file->add('foo.txt');
-var_dump($resourceObj);
-// Get status
-var_dump($file->status($resourceObj['uuid']));
+$apiUrl = 'https://your-sandbox.com/api';
+
+// Scan a file
+$file = new Cuckoo\File($apiKey);
+$return = $file->scan('foo.txt');
+var_dump($return);
+
+// Rescan a file
+$rescan_result = $file->rescan($return['id']);
+
+// Search for a report by hash
+$hash = hash_file('sha256', 'foo.txt');
+$report = $file->searchReport($hash);
+var_dump($report);
